62cc137a8c34bbe74da2f24d35929b6aa65f9010
REZWANULLAH QURAISHI <rezwankhanq@gmail.com>
2024-03-23 23:50:55 +0300

    6.Week DONE

--- Weeks/5.Week/5week_OOP_expls.py
+++ Weeks/5.Week/5week_OOP_expls.py
@@ -95,103 +95,25 @@ print("a / b:", a / b)
 
 # Example 6. Collatz Problem: Start with a number n > 1. Find the number of steps it takes to reach one
 # using the following process: If n is even, divide it by 2. If n is odd, multiply it by 3 and add I.
-#
-# def CollatzProblem(num):
-#     steps = 0
-#     while num > 1:
-#         print(num)  # Print the current number
-#         if num % 2 == 0:
-#             num = num / 2
-#         else:
-#             num = (num * 3) + 1
-#         steps += 1
-#     print(num)  # Print the final number (which is 1)
-#     return steps
-#
-#
-# try:
-#     start_number = int(input("Enter a number greater than 1: "))
-#     if start_number <= 1:
-#         raise ValueError("Input must be greater than 1")
-#     steps_taken = CollatzProblem(start_number)
-#     print("Number of steps taken to reach 1 starting from", start_number, ":", steps_taken)
-# except ValueError as ve:
-#     print("Invalid input:", ve)
 
-"""
-Example 7. Caesar cipher
-The key is an integer from 1 to 25. This cipher rotates the letters of the alphabet (A to Z). The encoding
-replaces each letter with the 1st to 25th next letter in the alphabet (wrapping Z to A). So key = 2 encrypts
-"HI" to "JK", but key=20 encrypts "HI" to "BC".
-(Note: ASCII code starts from A=65, chr() is to get character from ASCII table, ord() is to get ASCII value
-of character. English alphabet has 26 letters).
-Encrypt the text="KARATAY" using key=4
-text = "KARATAY"
-key = 4
-print ("Text : " + text)
-print ("Shift : " + str(key))
-print ("Cipher: " + encrypt(text,key))"""
-
-
-def encrypt(text, key):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            # print(char)
-            # Determine whether the character is uppercase or lowercase
-            if char.isupper():
-                encrypted_char = chr((ord(char) - 65 + key) % 26 + 65)
-            else:
-                encrypted_char = chr((ord(char) - 97 + key) % 26 + 97)
-            encrypted_text += encrypted_char
+def CollatzProblem(num):
+    steps = 0
+    while num > 1:
+        print(num)  # Print the current number
+        if num % 2 == 0:
+            num = num / 2
         else:
-            encrypted_text += char  # Non-alphabetic characters remain unchanged
-    return encrypted_text
+            num = (num * 3) + 1
+        steps += 1
+    print(num)  # Print the final number (which is 1)
+    return steps
 
 
-text = "KARATAY"
-key = 1  # LBSBUBZ
-
-print("Text : " + text)
-print("Shift : " + str(key))
-print("Cipher: " + encrypt(text, key))
-
-
-
-
-
-class Student:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.skills = []
-
-    def studentinfo(self):
-        print("Student Name:", self.firstname)
-        print("Student Last Name:", self.lastname)
-        print(self.skills)
-
-    def Addskills(self, skill):
-      self.skills.append(skill)
-
-
-A = Student("john", "Root")
-A.Addskills("C")
-print(A.studentinfo())
-
-print("\nSalary Example")
-
-
-class Employee:
-
-    def __init__(self, name, salary):
-        self.name = name
-        self.__salary = salary
-
-    def showinfo(self):
-        print("Name: ", self.name)
-        print("Salary: ", self.__salary)
-
-
-Person1 = Employee("John", "8000$")
-Person1.showinfo()
+try:
+    start_number = int(input("Enter a number greater than 1: "))
+    if start_number <= 1:
+        raise ValueError("Input must be greater than 1")
+    steps_taken = CollatzProblem(start_number)
+    print("Number of steps taken to reach 1 starting from", start_number, ":", steps_taken)
+except ValueError as ve:
+    print("Invalid input:", ve)
---
+++ Weeks/6.Week/6week.OOP_Exmples.py
@@ -0,0 +1,79 @@
+
+"""
+Example 7. Caesar cipher
+The key is an integer from 1 to 25. This cipher rotates the letters of the alphabet (A to Z). The encoding
+replaces each letter with the 1st to 25th next letter in the alphabet (wrapping Z to A). So key = 2 encrypts
+"HI" to "JK", but key=20 encrypts "HI" to "BC".
+(Note: ASCII code starts from A=65, chr() is to get character from ASCII table, ord() is to get ASCII value
+of character. English alphabet has 26 letters).
+Encrypt the text="KARATAY" using key=4
+text = "KARATAY"
+key = 4
+print ("Text : " + text)
+print ("Shift : " + str(key))
+print ("Cipher: " + encrypt(text,key))"""
+
+
+def encrypt(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            # print(char)
+            # Determine whether the character is uppercase or lowercase
+            if char.isupper():
+                encrypted_char = chr((ord(char) - 65 + key) % 26 + 65)
+            else:
+                encrypted_char = chr((ord(char) - 97 + key) % 26 + 97)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char  # Non-alphabetic characters remain unchanged
+    return encrypted_text
+
+
+text = "KARATAY"
+key = 1  # LBSBUBZ
+
+print("Text : " + text)
+print("Shift : " + str(key))
+print("Cipher: " + encrypt(text, key))
+
+
+
+
+
+class Student:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.skills = []
+
+    def studentinfo(self):
+        print("Student Name:", self.firstname)
+        print("Student Last Name:", self.lastname)
+        print(self.skills)
+
+    def Addskills(self, skill):
+      self.skills.append(skill)
+
+
+A = Student("john", "Root")
+A.Addskills("C")
+print(A.studentinfo())
+
+
+print("\nSalary Example")
+
+
+class Employee:
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.__salary = salary
+
+    def showinfo(self):
+        print("Name: ", self.name)
+        print("Salary: ", self.__salary)
+
+
+Person1 = Employee("John", "8000$")
+Person1.showinfo()
